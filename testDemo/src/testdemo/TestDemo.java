46879aa564080d21ced5d9c01c926b8bf6b63825
jawadaminkp <59015763+jawadaminkp@users.noreply.github.com>
2020-12-16 12:38:17 +0500

    I have added an sout statement

---
+++ testDemo/src/testdemo/TestDemo.java
@@ -0,0 +1,17 @@
+
+package testdemo;
+
+/**
+ *
+ * @author Spogmay Jawad
+ */
+public class TestDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub");
+    }
+    
+}
